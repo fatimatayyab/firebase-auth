3814bc083613991872b8a27da5ef065d26cad7e9
fatimatayyab <fatima.idrees.1993@gmail.com>
2023-08-17 09:56:24 +0500

    step-2

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:testproject/views/login_view.dart';
 import 'package:testproject/views/register_view.dart';
+import 'package:testproject/views/verify_email_view.dart';
 import 'firebase_options.dart';
 
 void main() {
@@ -17,8 +18,8 @@ void main() {
       ),
       home: const HomePage(),
       routes: {
-        '/login/' : (context) =>  const LoginView(),
-        '/register/' : (context) => const RegisterView(),
+        '/login/': (context) => const LoginView(),
+        '/register/': (context) => const RegisterView(),
       },
     ),
   );
@@ -30,28 +31,28 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              // final user = FirebaseAuth.instance.currentUser;
-              // if (user?.emailVerified ?? false) {
-              //   return const Text("done");
-              // } else {
-              //   return const VerifyEmailView();
-              // }
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      ),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print("Email is Verified");
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
               return const LoginView();
-            default:
-              return const Text("Loading..");
-          }
-        },
-  
-      );
-      
+            }
+            return const Text('Done');
+            
+          default:
+            return const Text("Loading..");
+        }
+      },
+    );
   }
-
 }
-
-
