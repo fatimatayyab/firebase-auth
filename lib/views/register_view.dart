43284c64ee9ce7fccca4f7fd41676499b45b943d
fatimatayyab <fatima.idrees.1993@gmail.com>
2023-08-28 10:28:47 +0500

    step-5

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
 
+import 'package:testproject/constants/routes.dart';
+
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
 
@@ -72,8 +74,8 @@ class _RegisterViewState extends State<RegisterView> {
           ),
           TextButton(
               onPressed: () {
-                Navigator.of(context).pushNamedAndRemoveUntil(
-                  '/login/',
+                Navigator.of(context).NamedAndRemoveUntil(
+                  loginRoute,
                   (route) => false,
                 );
               },
